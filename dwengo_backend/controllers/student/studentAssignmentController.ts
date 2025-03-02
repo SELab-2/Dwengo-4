ee8bd8aaf77fbcc3134cf0923e6f7c29777f00e9
hendrik <hendrik.decoster@ugent.be>
2025-03-02 15:21:09 +0100

    Crud taken leerlingen #28 Databankschema aangepast 3 nieuwe files

--- dwengo_backend/controllers/student/studentAssignmentController.ts
+++ dwengo_backend/controllers/student/studentAssignmentController.ts
@@ -1,21 +1,23 @@
 import { Request, Response } from "express";
 import { getAssignmentsForStudent, getAssignmentsWithClosestDeadlines } from "../../services/studentAssignmentService";
+import {Assignment} from "@prisma/client";
 
-export const getStudentAssignments = async (req: Request & { user: { id: number } }, res: Response) => {
+export const getStudentAssignments = async (req: Request, res: Response): Promise<void> => {
     try {
-        const studentId = req.user.id;
-        const assignments = await getAssignmentsForStudent(studentId);
+        const { studentId } = req.body;
+        const assignments: Assignment[] = await getAssignmentsForStudent(studentId);
         res.status(200).json(assignments);
     } catch (error) {
         res.status(500).json({ error: "Er is iets misgegaan bij het ophalen van de taken." });
     }
 };
 
-export const getClosestDeadlines = async (req: Request & { user: { id: number } }, res: Response) => {
+export const getClosestDeadlines = async (req: Request, res: Response): Promise<void> => {
     try {
-        const studentId = req.user.id;
-        const limit = Number(req.query.limit) || 5; // Standaard 5 taken ophalen
-        const assignments = await getAssignmentsWithClosestDeadlines(studentId, limit);
+        const { studentId } = req.body;
+        // Get the 5 most urgent task unless specified otherwise
+        const limit: number = Number(req.query.limit) || 5;
+        const assignments: Assignment[] = await getAssignmentsWithClosestDeadlines(studentId, limit);
         res.status(200).json(assignments);
     } catch (error) {
         res.status(500).json({ error: "Er is iets misgegaan bij het ophalen van de taken met dichtsbijzijnde deadline." });
