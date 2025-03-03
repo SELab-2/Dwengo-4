5c17ef54c848de4a6eade424cafc8d9dc2424d16
hendrik <hendrik.decoster@ugent.be>
2025-03-03 12:17:49 +0100

    dubbele // in route weg

--- dwengo_backend/routes/joinrequest/joinRequestRoutes.ts
+++ dwengo_backend/routes/joinrequest/joinRequestRoutes.ts
@@ -6,8 +6,8 @@ import { isTeacher } from "../../middleware/teacherAuthMiddleware"
 const router = express.Router();
 
 router.post("/", joinRequestController.createJoinRequest);
-router.patch("//:classId/approve/:studentId", isTeacher, joinRequestController.approveJoinRequest);
-router.patch("//:classId/deny/:studentId", isTeacher, joinRequestController.denyJoinRequest);
+router.patch("/:classId/approve/:studentId", isTeacher, joinRequestController.approveJoinRequest);
+router.patch("/:classId/deny/:studentId", isTeacher, joinRequestController.denyJoinRequest);
 router.get("/class/:classId", isTeacher, joinRequestController.getJoinRequestsByClass);
 
 export default router;
