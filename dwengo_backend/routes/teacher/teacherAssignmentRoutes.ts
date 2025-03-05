526c512552bf60fa64c8c68966ca54f3bc143d81
hendrik <hendrik.decoster@ugent.be>
2025-03-05 11:12:13 +0100

    router: Router type

--- dwengo_backend/routes/teacher/teacherAssignmentRoutes.ts
+++ dwengo_backend/routes/teacher/teacherAssignmentRoutes.ts
@@ -1,7 +1,7 @@
-import express from 'express';
+import express, {Router} from 'express';
 import { AssignmentTeacherController } from "../../controllers/teacher/teacherAssignmentController";
 
-const router = express.Router();
+const router: Router = express.Router();
 const controller = new AssignmentTeacherController();
 
 router.post("/assignments", controller.createAssignmentForClass);
