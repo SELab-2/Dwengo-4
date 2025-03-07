03e843f8280e0c9b75f00ad192354a97743ce1c5
hendrik <hendrik.decoster@ugent.be>
2025-03-07 13:35:58 +0100

    parseInt beschermd

--- dwengo_backend/controllers/assignmentController.ts
+++ dwengo_backend/controllers/assignmentController.ts
@@ -4,6 +4,10 @@ import assignmentService from "../services/assignmentService";
 export class AssignmentController {
     async getAssignmentsById(req: Request, res: Response): Promise<void> {
         try {
+            if (!req.params.classId || isNaN(parseInt(req.params.classId))) {
+                res.status(400).json({ error: "Invalid class ID" });
+                return;
+            }
             const assignmentId: number = parseInt(req.params.classId);
             const assignments = await assignmentService.getAssignmentById(assignmentId);
             res.status(200).json(assignments);
