0b2416ce50d339841c12e3d3b330b642cd49c9cc
anewdt <annette.demeester@ugent.be>
2025-03-08 20:40:02 +0100

    trying to resolve merge conflict in index.ts

--- dwengo_backend/index.ts
+++ dwengo_backend/index.ts
@@ -1,10 +1,8 @@
-import  express from 'express';
-import { Request, Response, NextFunction } from 'express';
-
-import dotenv from 'dotenv';
-import errorHandler from './middleware/errorMiddleware';
-import teacherAuthRoutes from './routes/teacher/teacherAuthRoutes';
-import studentAuthRoutes from './routes/student/studentAuthRoutes';
+import express, { Request, Response, NextFunction } from "express";
+import dotenv from "dotenv";
+import errorHandler from "./middleware/errorMiddleware";
+import teacherAuthRoutes from "./routes/teacher/teacherAuthRoutes";
+import studentAuthRoutes from "./routes/student/studentAuthRoutes";
 import teacherClassRoutes from './routes/teacher/teacherClassRoutes';
 
 dotenv.config();
@@ -13,35 +11,32 @@ const app = express();
 
 // Stel CORS-headers in
 app.use((req: Request, res: Response, next: NextFunction) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
+  const allowedOrigins = ["https://dwengo.org", "http://localhost:3000"];
+  const origin = req.headers.origin;
+  if (origin && allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorization");
   next();
 });
 
 // JSON-parser middleware
 app.use(express.json());
 
-// Hello World route
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, World!');
-});
 
-app.get('/login', (req: Request, res: Response) => {
-  res.send('Hello, World 3!');
-});
+// Routes voor Teacher (Auth)
+app.use("/teacher/auth", teacherAuthRoutes);
 
-// Routes voor Teacher
-app.use('/teacher/auth', teacherAuthRoutes);
+// Routes voor Student (Auth)
+app.use("/student/auth", studentAuthRoutes)
+
+// Routes voor Teacher (Classes)
 app.use("/teacher/classes", teacherClassRoutes);
 
-// Routes voor Student
-app.use('/student/auth', studentAuthRoutes);
-
-// Error handling middleware
+// Error Handler
 app.use(errorHandler);
 
-// Start server
 const PORT: string | number = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server draait op poort ${PORT}`));
 
