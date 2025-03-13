45bb0f16da40a55aada35957f0e9ce011ed911a0
Annette De Meester <137805016+anewdt@users.noreply.github.com>
2025-03-13 14:45:14 +0100

    reset db after running tests
    Co-authored-by: Senne Van den Berge <senne.vandenberge@ugent.be>

--- dwengo_backend/tests/helpers/setup.ts
+++ dwengo_backend/tests/helpers/setup.ts
@@ -2,5 +2,11 @@ import { beforeEach } from 'vitest'
 import resetDb from './reset-db'
 
 beforeEach(async () => {
+  await resetDb();
+});
+
+afterAll(async () => {
+  await resetDb();
+});
   await resetDb()
 })
