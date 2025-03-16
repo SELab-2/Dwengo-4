71a915810d5d7553808f0c6256afa79c702f4b36
Cédric Van Cauwenberghe <Cedric.VanCauwenberghe@UGent.be>
2025-03-16 12:34:17 +0100

    Leading slashes

--- dwengo_backend/routes/student/studentSubmissionRoute.ts
+++ dwengo_backend/routes/student/studentSubmissionRoute.ts
@@ -4,9 +4,9 @@ import {protectStudent} from "../../middleware/studentAuthMiddleware";
 
 const router: Router = express.Router();
 
-router.get('assignment/:assignmentId/', protectStudent, controller.getSubmissionsForAssignment)
+router.get('/assignment/:assignmentId/', protectStudent, controller.getSubmissionsForAssignment)
 
-router.post('assignment/:assignmentId/evaluation/:evaluationId', protectStudent, controller.createSubmission)
-router.get('assignment/:assignmentId/evaluation/:evaluationId', protectStudent, controller.getSubmissionsForEvaluation)
+router.post('/assignment/:assignmentId/evaluation/:evaluationId', protectStudent, controller.createSubmission)
+router.get('/assignment/:assignmentId/evaluation/:evaluationId', protectStudent, controller.getSubmissionsForEvaluation)
 
 export default router;
--- dwengo_backend/routes/teacher/teacherSubmissionRoute.ts
+++ dwengo_backend/routes/teacher/teacherSubmissionRoute.ts
@@ -4,11 +4,11 @@ import {protectTeacher} from "../../middleware/teacherAuthMiddleware";
 
 const router: Router = express.Router();
 
-router.get('student/:studentId', protectTeacher, controller.getSubmissionsForStudent);
-router.get('team/:teamId', protectTeacher, controller.getSubmissionsForTeam);
+router.get('/student/:studentId', protectTeacher, controller.getSubmissionsForStudent);
+router.get('/team/:teamId', protectTeacher, controller.getSubmissionsForTeam);
 
-router.get('assignment/:assignmentId/student/:studentId', protectTeacher, controller.getAssignmentSubmissionsForStudent);
-router.get('assignment/:assignmentId/team/:teamId', protectTeacher, controller.getAssignmentSubmissionsForTeam);
+router.get('/assignment/:assignmentId/student/:studentId', protectTeacher, controller.getAssignmentSubmissionsForStudent);
+router.get('/assignment/:assignmentId/team/:teamId', protectTeacher, controller.getAssignmentSubmissionsForTeam);
 
 
 export default router;
