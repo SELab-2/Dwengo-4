76177fbd992a65b8617d09812c42351f924452d0
hendrik <hendrik.decoster@ugent.be>
2025-03-18 11:20:02 +0100

    Moest routes van teacherTeamsRoutes.ts aanpassen om overlap te vermijden

--- dwengo_backend/routes/teacher/teacherTeamsRoutes.ts
+++ dwengo_backend/routes/teacher/teacherTeamsRoutes.ts
@@ -1,37 +1,55 @@
-import express, {Router} from "express";
+import express, { Router } from "express";
 import {
-    createTeamInAssignment,
-    getTeamsInAssignment,
-    updateTeamsInAssignment,
-    deleteTeamInAssignment
+  createTeamInAssignment,
+  getTeamsInAssignment,
+  updateTeamsInAssignment,
+  deleteTeamInAssignment,
 } from "../../controllers/teacher/teacherTeamsController";
 import {
-    makeAssignmentIdParamValid,
-    makeTeamIdParamValid,
-    ensureTeamsParamValidTeamDivision,
-    ensureTeamParamValidIdentifiableTeamDivision
+  makeAssignmentIdParamValid,
+  makeTeamIdParamValid,
+  ensureTeamsParamValidTeamDivision,
+  ensureTeamParamValidIdentifiableTeamDivision,
 } from "../../middleware/teamValidationMiddleware";
-import {protectTeacher} from "../../middleware/teacherAuthMiddleware";
+import { protectTeacher } from "../../middleware/teacherAuthMiddleware";
 
-
-const router: Router = express.Router({ mergeParams: true });
+const router: Router = express.Router();
 
 // Prefix voor deze routes:
-// "/teacher/assignments/:assignmentId/team"
-
-// Route prefix
-// "/teacher/:assignmentId"
+// "/teacher/team/assignments/:assignmentId"
 
 // Route to create teams in an assignment
-router.post("/class/:classId", protectTeacher, makeAssignmentIdParamValid, ensureTeamsParamValidTeamDivision, createTeamInAssignment);
+router.post(
+  "class/:classId",
+  protectTeacher,
+  makeAssignmentIdParamValid,
+  ensureTeamsParamValidTeamDivision,
+  createTeamInAssignment
+);
 
 // Route to get all teams in an assignment
-router.get("/", protectTeacher, makeAssignmentIdParamValid, getTeamsInAssignment);
+router.get(
+  "/",
+  protectTeacher,
+  makeAssignmentIdParamValid,
+  getTeamsInAssignment
+);
 
 // Route to update teams in an assignment
-router.patch("/", protectTeacher, makeAssignmentIdParamValid, ensureTeamParamValidIdentifiableTeamDivision, updateTeamsInAssignment);
+router.patch(
+  "/",
+  protectTeacher,
+  makeAssignmentIdParamValid,
+  ensureTeamParamValidIdentifiableTeamDivision,
+  updateTeamsInAssignment
+);
 
 // Route to delete a team in an assignment
-router.delete("/:teamId", protectTeacher, makeTeamIdParamValid, deleteTeamInAssignment);
+router.delete(
+  "/:teamId",
+  protectTeacher,
+  makeTeamIdParamValid,
+  deleteTeamInAssignment
+);
 
 export default router;
