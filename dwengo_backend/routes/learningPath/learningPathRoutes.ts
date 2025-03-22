8173935bc88b5fcbf4fa64ab6418768260791a2b
senne <senne.vandenberge@ugent.be>
2025-03-22 15:52:11 +0100

    comments bij leerpaden

--- dwengo_backend/routes/learningPath/learningPathRoutes.ts
+++ dwengo_backend/routes/learningPath/learningPathRoutes.ts
@@ -12,16 +12,38 @@ const router: Router = express.Router();
 // Kan zowel een student als een teacher zijn.
 router.use(protectAnyUser);
 
-// GET /teacher/learningPath?language=nl&hruid=...&title=...&description=...&all=
-// GET /student/learningPath?language=nl&hruid=...&title=...&description=...&all=
+/**
+ * @route GET /teacher/learningPath?language=nl&hruid=...&title=...&description=...&all=
+ * @route GET /student/learningPath?language=nl&hruid=...&title=...&description=...&all=
+ * @description Zoek naar leerpaden op basis van de query parameters
+ * @queryparam language: string
+ * @queryparam hruid: string
+ * @queryparam title: string
+ * @queryparam description: string
+ * @queryparam all: string
+ * @access User
+ */
 router.get("/", searchLearningPathsController);
 
-// GET /teacher/learningPath/:pathId
-// GET /student/learningPath/:pathId
+/**
+ * @route GET /teacher/learningPath/:pathId
+ * @route GET /student/learningPath/:pathId
+ * @description Haal 1 leerpad op (op basis van _id of hruid)
+ * @param pathId: string
+ * @access User
+ */
 router.get("/:pathId", getLearningPathByIdController);
 
-// GET /teacher/learningPath/:pathId/learningObject --> Haal alle leerobjecten op die horen bij een specifiek leerpad
-// GET /student/learningPath/:pathId/learningObject
+/**
+ * @route GET /teacher/learningPath/:pathId/learningObject
+ * @route GET /student/learningPath/:pathId/learningObject
+ * @description Haal alle leerobjecten op die horen bij een specifiek leerpad
+ * @param pathId: string
+ * @queryparam language: string
+ * @queryparam hruid: string
+ * @queryparam version: int
+ * @access User
+ */
 router.get("/learningObject", getLearningObjectByHruidLangVersionController);
 
 export default router;
