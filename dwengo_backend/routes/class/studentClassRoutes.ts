566105d8465f78f0ffbb076caa87e50d3a635a41
senne <senne.vandenberge@ugent.be>
2025-03-23 09:27:50 +0100

    comments studentClassRoutes toegevoegd

--- dwengo_backend/routes/class/studentClassRoutes.ts
+++ dwengo_backend/routes/class/studentClassRoutes.ts
@@ -8,7 +8,19 @@ const router = express.Router();
 // Alleen studenten mogen deze route gebruiken
 router.use(protectStudent);
 
+/**
+ * @route POST /class/join
+ * @description Create a join request for a class
+ * @body joinCode: number
+ * @access Student
+ */
 router.post("/join", createJoinRequest);
+
+/**
+ * @route GET /class/student
+ * @description Get all classes for a student
+ * @access Student
+ */
 router.get("/student", getStudentClasses);
 
 export default router;
