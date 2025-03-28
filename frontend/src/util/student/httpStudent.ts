3a001fbcd5a78746bd862d73280e42d7e613596c
Cédric Van Cauwenberghe <Cedric.VanCauwenberghe@UGent.be>
2025-03-28 19:34:25 +0100

    Ophalen assignments

--- frontend/src/util/student/httpStudent.ts
+++ frontend/src/util/student/httpStudent.ts
@@ -115,16 +115,16 @@ export interface ClassItem {
 
 export async function fetchClasses(): Promise<ClassItem[]> {
   const response = await fetch(`${BACKEND}/student/classes`, {
-    method: "GET",
+    method: 'GET',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
       Authorization: `Bearer ${getAuthToken()}`,
     },
   });
 
   if (!response.ok) {
     const error: APIError = new Error(
-        "Er is iets misgegaan bij het ophalen van de klassen."
+      'Er is iets misgegaan bij het ophalen van de klassen.',
     );
     error.code = response.status;
     error.info = await response.json();
@@ -132,8 +132,50 @@ export async function fetchClasses(): Promise<ClassItem[]> {
   }
 
   const returner = await response.json();
-  const classrooms = returner["classrooms"];
-  console.log("GETTING CLASSES", classrooms);
+  const classrooms = returner['classrooms'];
+  console.log('GETTING CLASSES', classrooms);
 
   return await classrooms;
+}
+
+export interface AssignmentItem {
+  id: string;
+  title: string;
+  description: string;
+  deadline: string;
+}
+
+export async function fetchAssignments(): Promise<AssignmentItem[]> {
+  const response = await fetch(`${BACKEND}/student/assignments`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${getAuthToken()}`,
+    },
+  });
+
+  if (!response.ok) {
+    const error: APIError = new Error(
+      'Er is iets misgegaan bij het ophalen van de taken.',
+    );
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const returner = await response.json();
+  const assignments = returner['assignments'];
+
+  return await assignments;
+  // return [{
+  //     id: '15',
+  //     title: 'Test',
+  //     description: 'Chew foot twitch tail in permanent irritation or play with\n' +
+  //         '                                        twist ties when owners are asleep, cry for no apparent\n' +
+  //         '                                        reason. Meow all night shove bum in owner\'s face like camera\n' +
+  //         '                                        lens check cat door for ambush 10 times before coming in yet\n' +
+  //         '                                        jump up to edge of bath, fall in then scramble in a mad\n' +
+  //         '                                        panic to get out cats are the world. Human is washing you',
+  //     deadline: '23/02/2025' // TODO hoe komt dit uit de database?
+  // }];
 }
