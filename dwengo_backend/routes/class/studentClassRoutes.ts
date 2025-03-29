53582f3e1fa9f62570cebeaff1509be215b3bbc3
senne <senne.vandenberge@ugent.be>
2025-03-29 14:53:43 +0100

    class routes added

--- dwengo_backend/routes/class/studentClassRoutes.ts
+++ dwengo_backend/routes/class/studentClassRoutes.ts
@@ -1,17 +1,8 @@
 import express from "express";
 import { protectStudent } from "../../middleware/studentAuthMiddleware";
-import { createJoinRequest } from "../../controllers/joinrequest/joinRequestController";
 import { getStudentClasses } from "../../controllers/student/studentClassController";
 
 const router = express.Router();
-
-/**
- * @route POST /class/student/join
- * @description Create a join request for a class
- * @body joinCode: number
- * @access Student
- */
-router.post("/join", protectStudent, createJoinRequest);
 
 /**
  * @route GET /class/student
