35d5ab8593ee84693ec86a752f516d75554c3dc4
senne <senne.vandenberge@ugent.be>
2025-03-30 17:31:38 +0200

    class service works with central error handling and prisma request handler

--- dwengo_backend/services/classService.ts
+++ dwengo_backend/services/classService.ts
@@ -12,6 +12,7 @@ import {
   BadRequestError,
   NotFoundError,
 } from "../errors/errors";
+import handlePrismaQuery from "../errors/prismaErrorHandler";
 
 const prisma = new PrismaClient();
 
@@ -22,20 +23,24 @@ export default class ClassService {
     // Generate a unique join code (e.g., an 8-digit hex string)
     const joinCode = await this.generateUniqueCode();
 
-    const classroom = await prisma.class.create({
-      data: {
-        name: name, // name field matches the Class schema
-        code: joinCode, // code field matches the Class schema
-      },
-    });
+    const classroom = await handlePrismaQuery(() =>
+      prisma.class.create({
+        data: {
+          name: name, // name field matches the Class schema
+          code: joinCode, // code field matches the Class schema
+        },
+      }),
+    );
 
     // Now that you have the class id, create the ClassTeacher record
-    await prisma.classTeacher.create({
-      data: {
-        teacherId: teacherId, // teacherId links to the teacher record
-        classId: classroom.id, // Use the id of the newly created class
-      },
-    });
+    await handlePrismaQuery(() =>
+      prisma.classTeacher.create({
+        data: {
+          teacherId: teacherId, // teacherId links to the teacher record
+          classId: classroom.id, // Use the id of the newly created class
+        },
+      }),
+    );
 
     return classroom;
   }
@@ -44,21 +49,23 @@ export default class ClassService {
   // if either of these conditions are not met, an error is thrown
   private static async verifyClassAndTeacher(
     classId: number,
-    teacherId: number
+    teacherId: number,
   ): Promise<void> {
     // Check if the class exists
-    const classroom = await prisma.class.findUnique({
-      where: { id: classId },
-    });
+    const classroom = await handlePrismaQuery(() =>
+      prisma.class.findUnique({
+        where: { id: classId },
+      }),
+    );
     if (!classroom) {
-      throw new NotFoundError(`Class with id ${classId} not found`);
+      throw new NotFoundError(`Class with id ${classId} not found.`);
     }
 
     // Check if the teacher is associated with the class
     const isTeacher = await this.isTeacherOfClass(classId, teacherId);
     if (!isTeacher) {
       throw new AccesDeniedError(
-        `Acces denied: Teacher ${teacherId} is not part of class ${classId}`
+        `Acces denied: Teacher ${teacherId} is not part of class ${classId}.`,
       );
     }
     return;
@@ -67,183 +74,206 @@ export default class ClassService {
   // Delete a class by ID
   static async deleteClass(classId: number, teacherId: number): Promise<Class> {
     await this.verifyClassAndTeacher(classId, teacherId);
-    return await prisma.class.delete({ where: { id: classId } }); // onDelete: Cascade in the prisma schema makes sure that all related records are also deleted
+    return await handlePrismaQuery(() =>
+      prisma.class.delete({ where: { id: classId } }),
+    ); // onDelete: Cascade in the prisma schema makes sure that all related records are also deleted
   }
 
   // Get all classes taught by a given teacher
   static async getClassesByTeacher(teacherId: number): Promise<Class[]> {
     // Fetch all classes where the teacher is assigned
-    return prisma.class.findMany({
-      where: {
-        ClassTeacher: {
-          some: {
-            teacherId: teacherId, // Filter for the given teacherId
+    return handlePrismaQuery(() =>
+      prisma.class.findMany({
+        where: {
+          ClassTeacher: {
+            some: {
+              teacherId: teacherId, // Filter for the given teacherId
+            },
           },
         },
-      },
-    });
+      }),
+    );
   }
 
   // Get all classes a student is partaking in
   static async getClassesByStudent(studentId: number): Promise<Class[]> {
     // Fetch all classes where the student is enrolled
-    return prisma.class.findMany({
-      where: {
-        classLinks: {
-          some: {
-            studentId: studentId, // Filter for the given studentId
+    return await handlePrismaQuery(() =>
+      prisma.class.findMany({
+        where: {
+          classLinks: {
+            some: {
+              studentId: studentId, // Filter for the given studentId
+            },
           },
         },
-      },
-    });
+      }),
+    );
   }
 
   // Function to check if the requester is the teacher of the class
   static async isTeacherOfClass(
     classId: number,
-    teacherId: number
+    teacherId: number,
   ): Promise<boolean> {
-    const classTeacher: ClassTeacher | null =
-      await prisma.classTeacher.findUnique({
+    const classTeacher: ClassTeacher | null = await handlePrismaQuery(() =>
+      prisma.classTeacher.findUnique({
         where: {
           teacherId_classId: {
             teacherId: teacherId,
             classId: classId,
           },
         },
-      });
+      }),
+    );
     return classTeacher !== null;
   }
 
   // Get all students from a given class
   static async getStudentsByClass(
     classId: number,
-    teacherId: number
+    teacherId: number,
   ): Promise<(Student & { user: User })[]> {
     await this.verifyClassAndTeacher(classId, teacherId);
 
-    const classStudents = await prisma.classStudent.findMany({
-      where: { classId },
-      include: {
-        student: {
-          include: { user: true }, // include user details
+    const classStudents = await handlePrismaQuery(() =>
+      prisma.classStudent.findMany({
+        where: { classId },
+        include: {
+          student: {
+            include: { user: true }, // include user details
+          },
         },
-      },
-    });
+      }),
+    );
     return classStudents.map((cs) => cs.student);
   }
 
   static async addStudentToClass(
     studentId: number,
-    classId: number
+    classId: number,
   ): Promise<ClassStudent> {
-    return prisma.classStudent.create({
-      data: {
-        studentId,
-        classId,
-      },
-    });
+    return handlePrismaQuery(() =>
+      prisma.classStudent.create({
+        data: {
+          studentId,
+          classId,
+        },
+      }),
+    );
   }
 
   // Check if student is already in the class
   static async isStudentInClass(
     classroom: ClassWithLinks,
-    studentId: number
+    studentId: number,
   ): Promise<boolean> {
     return classroom.classLinks.some(
-      (link: ClassStudent) => link.studentId === studentId
+      (link: ClassStudent) => link.studentId === studentId,
     );
   }
 
   static async removeStudentFromClass(
     studentId: number,
-    classId: number
+    classId: number,
   ): Promise<ClassStudent> {
-    return prisma.classStudent.delete({
-      where: {
-        studentId_classId: {
-          studentId,
-          classId,
+    return handlePrismaQuery(() =>
+      prisma.classStudent.delete({
+        where: {
+          studentId_classId: {
+            studentId,
+            classId,
+          },
         },
-      },
-    });
+      }),
+    );
   }
 
   // Return all classes
   static async getAllClasses(): Promise<Class[]> {
     // Returns an empty array if nothing is found
-    return prisma.class.findMany();
+    return await handlePrismaQuery(() => prisma.class.findMany());
   }
 
   // Read a class by ID
   static async getClassById(id: number): Promise<Class | null> {
-    return prisma.class.findUnique({
-      where: { id: id },
-    });
+    return await handlePrismaQuery(() =>
+      prisma.class.findUnique({
+        where: { id: id },
+      }),
+    );
   }
 
   // Read a class by ID and teacher ID
   static async getClassByIdAndTeacherId(
     classId: number,
-    teacherId: number
+    teacherId: number,
   ): Promise<Class | null> {
     // Verify if the teacher is associated with the class
     const isTeacher = await this.isTeacherOfClass(classId, teacherId);
     if (!isTeacher) {
       throw new AccesDeniedError(
-        `Access denied: Teacher ${teacherId} is not part of class ${classId}`
+        `Access denied: Teacher ${teacherId} is not part of class ${classId}.`,
       );
     }
 
     // Fetch the class by ID
-    return prisma.class.findUnique({
-      where: { id: classId },
-    });
+    return await handlePrismaQuery(() =>
+      prisma.class.findUnique({
+        where: { id: classId },
+      }),
+    );
   }
 
   // Give the class a new name
   static async updateClassName(
     classId: number,
-    newName: string
+    newName: string,
   ): Promise<Class> {
-    return prisma.class.update({
-      where: { id: classId },
-      data: { name: newName },
-    });
+    return await handlePrismaQuery(() =>
+      prisma.class.update({
+        where: { id: classId },
+        data: { name: newName },
+      }),
+    );
   }
 
   // Read a class by JoinCode
   static async getClassByJoinCode(
-    joinCode: string
+    joinCode: string,
   ): Promise<ClassWithLinks | null> {
     if (!joinCode) {
       throw new BadRequestError(`Invalid join code: ${joinCode}`);
     }
-    return prisma.class.findUnique({
-      where: { code: joinCode }, // Search by the joinCode (which is 'code' in the schema)
-      include: {
-        classLinks: true,
-      },
-    });
+    return await handlePrismaQuery(() =>
+      prisma.class.findUnique({
+        where: { code: joinCode }, // Search by the joinCode (which is 'code' in the schema)
+        include: {
+          classLinks: true,
+        },
+      }),
+    );
   }
 
   static async getJoinCode(
     classId: number,
-    teacherId: number
+    teacherId: number,
   ): Promise<string> {
-    const classroom = await prisma.class.findUnique({
-      where: { id: classId },
-      select: { code: true }, // Only fetch the join code
-    });
+    const classroom = await handlePrismaQuery(() =>
+      prisma.class.findUnique({
+        where: { id: classId },
+        select: { code: true }, // Only fetch the join code
+      }),
+    );
     if (!classroom) {
-      throw new NotFoundError(`Class with id ${classId} not found`);
+      throw new NotFoundError(`Class with id ${classId} not found.`);
     }
 
     // Check if the teacher is associated with the class
     const isTeacher = await this.isTeacherOfClass(classId, teacherId);
     if (!isTeacher) {
       throw new AccesDeniedError(
-        `Acces denied: Teacher ${teacherId} is not part of class ${classId}`
+        `Acces denied: Teacher ${teacherId} is not part of class ${classId}`,
       );
     }
 
@@ -260,9 +290,11 @@ export default class ClassService {
       newJoinCode = crypto.randomBytes(4).toString("hex");
 
       // Check if the code is unique in the database
-      const existingClass = await prisma.class.findUnique({
-        where: { code: newJoinCode },
-      });
+      const existingClass = await handlePrismaQuery(() =>
+        prisma.class.findUnique({
+          where: { code: newJoinCode },
+        }),
+      );
 
       if (!existingClass) {
         isUnique = true;
@@ -275,7 +307,7 @@ export default class ClassService {
   // Function to regenerate the join code for a class
   static async regenerateJoinCode(
     classId: number,
-    teacherId: number
+    teacherId: number,
   ): Promise<string> {
     await this.verifyClassAndTeacher(classId, teacherId);
 
@@ -283,10 +315,12 @@ export default class ClassService {
     const newJoinCode = await this.generateUniqueCode();
 
     // Update class with the new join code
-    const updatedClass = await prisma.class.update({
-      where: { id: classId },
-      data: { code: newJoinCode },
-    });
+    const updatedClass = await handlePrismaQuery(() =>
+      prisma.class.update({
+        where: { id: classId },
+        data: { code: newJoinCode },
+      }),
+    );
 
     return updatedClass.code;
   }
@@ -294,14 +328,16 @@ export default class ClassService {
   // Get all classes from the same teacher
   static async getAllClassesByTeacher(teacherId: number): Promise<Class[]> {
     // Fetch all classes taught by the same teacher
-    return prisma.class.findMany({
-      where: {
-        ClassTeacher: {
-          some: {
-            teacherId,
+    return handlePrismaQuery(() =>
+      prisma.class.findMany({
+        where: {
+          ClassTeacher: {
+            some: {
+              teacherId,
+            },
           },
         },
-      },
-    });
+      }),
+    );
   }
 }
