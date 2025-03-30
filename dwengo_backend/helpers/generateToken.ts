9c79fa865a62305afa6c548d253266ca3b38a6ad
senne <senne.vandenberge@ugent.be>
2025-03-30 10:31:34 +0200

    works with central error handler

--- dwengo_backend/helpers/generateToken.ts
+++ dwengo_backend/helpers/generateToken.ts
@@ -1,9 +1,10 @@
 // Functie om een JWT-token te genereren
 import jwt from "jsonwebtoken";
+import { InternalServerError } from "../errors/errors";
 
 export const generateToken: (id: number | string) => string = (id: number | string): string => {
     if (!process.env.JWT_SECRET) {
-        throw new Error("JWT_SECRET is niet gedefinieerd in de omgevingsvariabelen");
+        throw new InternalServerError("JWT_SECRET is not defined in the evironment variables.");
     }
     return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "7d" });
 };
