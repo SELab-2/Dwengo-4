304f31ce3f0e2f2d03f57d734d49d2d2c7cffc38
senne <senne.vandenberge@ugent.be>
2025-03-30 16:44:15 +0200

    assignmentService changed

--- dwengo_backend/services/assignmentService.ts
+++ dwengo_backend/services/assignmentService.ts
@@ -1,13 +1,17 @@
 import { Assignment, PrismaClient } from "@prisma/client";
+import handlePrismaQuery from "../errors/prismaErrorHandler";
 
 const prisma = new PrismaClient();
 
 export default class AssignmentService {
-  static async getAssignmentById(assignmentId: number): Promise<Assignment | null> {
-    return prisma.assignment.findUnique({
-      where: { id: assignmentId },
-      // geen include meer, want there's no relation to learningPath
-    });
+  static async getAssignmentById(
+    assignmentId: number,
+  ): Promise<Assignment | null> {
+    return handlePrismaQuery(() =>
+      prisma.assignment.findUnique({
+        where: { id: assignmentId },
+        // geen include meer, want there's no relation to learningPath
+      }),
+    );
   }
 }
-
