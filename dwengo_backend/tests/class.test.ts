bedbc7db6a49365af8b493a83293bbf9345f6fb1
hendrik <hendrik.decoster@ugent.be>
2025-04-03 10:23:30 +0200

    opkuis class.test.ts

--- dwengo_backend/tests/class.test.ts
+++ dwengo_backend/tests/class.test.ts
@@ -2,7 +2,17 @@ import { beforeEach, describe, expect, it } from "vitest";
 import request from "supertest";
 import prisma from "./helpers/prisma";
 import app from "../index";
-import { Class, Prisma, Teacher, User } from "@prisma/client";
+import {
+  Class,
+  ClassAssignment,
+  ClassStudent,
+  ClassTeacher,
+  Invite,
+  JoinRequest,
+  Prisma,
+  Teacher,
+  User,
+} from "@prisma/client";
 import {
   addStudentToClass,
   addTeacherToClass,
@@ -13,21 +23,21 @@ import {
   createTeacher,
 } from "./helpers/testDataCreation";
 
-const APP_URL = process.env.APP_URL || "http://localhost:5000";
+const APP_URL: string = process.env.APP_URL || "http://localhost:5000";
 
 let teacherUser1: User & { teacher: Teacher; token: string };
 let classroom: Class;
 
-describe("classroom tests", () => {
-  beforeEach(async () => {
+describe("classroom tests", (): void => {
+  beforeEach(async (): Promise<void> => {
     // create a teacher
     teacherUser1 = await createTeacher("Bob", "Boons", "bob.boons@gmail.com");
     // create a class
     classroom = await createClass("5A", "ABCD");
   });
 
-  describe("[GET] /class/teacher", () => {
-    it("should respond with a `200` status code and a list of classes", async () => {
+  describe("[GET] /class/teacher", (): void => {
+    it("should respond with a `200` status code and a list of classes", async (): Promise<void> => {
       // add teacherUser1 to some classes
       await addTeacherToClass(teacherUser1.id, classroom.id);
       const classroom2: Class = await createClass("6A", "EFGH");
@@ -51,12 +61,15 @@ describe("classroom tests", () => {
         ]),
       );
     });
-    it("shouldn't allow a student to get the classes via the teacher route", async () => {
-      const studentUser = await createStudent(
+    it("shouldn't allow a student to get the classes via the teacher route", async (): Promise<void> => {
+      const studentUser: Prisma.UserGetPayload<{
+        include: { student: true };
+      }> & { token: string } = await createStudent(
         "Alice",
         "Anderson",
         "alan@gmail.com",
       );
+
       const { status, body } = await request(app)
         .get("/class/teacher")
         .set("Authorization", `Bearer ${studentUser.token}`);
@@ -67,10 +80,12 @@ describe("classroom tests", () => {
     });
   });
 
-  describe("[GET] /class/student", () => {
-    it("should respond with a `200` status code and a list of classes", async () => {
+  describe("[GET] /class/student", (): void => {
+    it("should respond with a `200` status code and a list of classes", async (): Promise<void> => {
       // create a student and add them to some classes
-      const studentUser = await createStudent(
+      const studentUser: Prisma.UserGetPayload<{
+        include: { student: true };
+      }> & { token: string } = await createStudent(
         "Alice",
         "Anderson",
         "alan@gmail.com",
@@ -97,7 +112,7 @@ describe("classroom tests", () => {
         ]),
       );
     });
-    it("shouldn't allow a teacher to get the classes via the student route", async () => {
+    it("shouldn't allow a teacher to get the classes via the student route", async (): Promise<void> => {
       const { status, body } = await request(app)
         .get("/class/student")
         .set("Authorization", `Bearer ${teacherUser1.token}`);
@@ -108,8 +123,8 @@ describe("classroom tests", () => {
     });
   });
 
-  describe("[POST] /class/teacher", () => {
-    it("should respond with a `201` status code and a created class", async () => {
+  describe("[POST] /class/teacher", (): void => {
+    it("should respond with a `201` status code and a created class", async (): Promise<void> => {
       const { status, body } = await request(app)
         .post("/class/teacher")
         .set("Authorization", `Bearer ${teacherUser1.token}`)
@@ -128,10 +143,10 @@ describe("classroom tests", () => {
       });
 
       expect(createdClassroom).toBeDefined();
-      const classTeacher = createdClassroom!.ClassTeacher[0];
+      const classTeacher: ClassTeacher = createdClassroom!.ClassTeacher[0];
       expect(classTeacher.teacherId).toBe(teacherUser1.id);
     });
-    it("should respond with a `400` status code and a message when no valid class name is provided", async () => {
+    it("should respond with a `400` status code and a message when no valid class name is provided", async (): Promise<void> => {
       const { status, body } = await request(app)
         .post("/class/teacher")
         .set("Authorization", `Bearer ${teacherUser1.token}`)
@@ -140,8 +155,10 @@ describe("classroom tests", () => {
       expect(status).toBe(400);
       expect(body.message).toBe("Vul een geldige klasnaam in");
     });
-    it("should not allow a student to create a class", async () => {
-      const studentUser = await createStudent(
+    it("should not allow a student to create a class", async (): Promise<void> => {
+      const studentUser: Prisma.UserGetPayload<{
+        include: { student: true };
+      }> & { token: string } = await createStudent(
         "Alice",
         "Anderson",
         "aaaaa@gmail.com",
@@ -156,13 +173,15 @@ describe("classroom tests", () => {
     });
   });
 
-  describe("[DELETE] /class/teacher/:classId", () => {
-    it("should respond with a `200` status code and a message when the class is deleted", async () => {
+  describe("[DELETE] /class/teacher/:classId", (): void => {
+    it("should respond with a `200` status code and a message when the class is deleted", async (): Promise<void> => {
       // add teacherUser1 to class, so we can test deleting it
       await addTeacherToClass(teacherUser1.id, classroom.id);
 
       // also add some records related to the class, so we can test if they all get deleted when the class is deleted
-      const studentUser1 = await createStudent(
+      const studentUser1: Prisma.UserGetPayload<{
+        include: { student: true };
+      }> & { token: string } = await createStudent(
         "Alice",
         "Anderson",
         "aaaaa@gmail.com",
@@ -170,10 +189,12 @@ describe("classroom tests", () => {
       await addStudentToClass(studentUser1.id, classroom.id);
       await prisma.classStudent
         .findMany({ where: { classId: classroom.id } })
-        .then((classStudents) => {
+        .then((classStudents: ClassStudent[]): void => {
           expect(classStudents.length).toBe(1);
         });
-      const studentUser2 = await createStudent(
+      const studentUser2: Prisma.UserGetPayload<{
+        include: { student: true };
+      }> & { token: string } = await createStudent(
         "Bob",
         "Baker",
         "bobbaker@gmail.com",
@@ -181,10 +202,12 @@ describe("classroom tests", () => {
       await createJoinRequest(studentUser2.id, classroom.id);
       await prisma.joinRequest
         .findMany({ where: { classId: classroom.id } })
-        .then((joinRequests) => {
+        .then((joinRequests: ClassStudent[]): void => {
           expect(joinRequests.length).toBe(1);
         });
-      const teacherUser2 = await createTeacher(
+      const teacherUser2: Prisma.UserGetPayload<{
+        include: { teacher: true };
+      }> & { token: string } = await createTeacher(
         "Charlie",
         "Chaplin",
         "char.ch@gmail.com",
@@ -192,7 +215,7 @@ describe("classroom tests", () => {
       await createInvite(teacherUser1.id, teacherUser2.id, classroom.id);
       await prisma.invite
         .findMany({ where: { classId: classroom.id } })
-        .then((invites) => {
+        .then((invites: Invite[]): void => {
           expect(invites.length).toBe(1);
         });
 
@@ -204,7 +227,7 @@ describe("classroom tests", () => {
       expect(status).toBe(200);
       expect(body.message).toBe(`Klas met id ${classroom.id} verwijderd`);
       // verify that class was deleted
-      const deletedClass = await prisma.class.findFirst({
+      const deletedClass: Class | null = await prisma.class.findFirst({
         where: { id: classroom.id },
       });
       expect(deletedClass).toBeNull();
@@ -212,31 +235,31 @@ describe("classroom tests", () => {
       // verify that all associated records were also deleted
       await prisma.classTeacher
         .findMany({ where: { classId: classroom.id } })
-        .then((classTeachers) => {
+        .then((classTeachers: ClassTeacher[]): void => {
           expect(classTeachers.length).toBe(0);
         });
       await prisma.classStudent
         .findMany({ where: { classId: classroom.id } })
-        .then((classStudents) => {
+        .then((classStudents: ClassStudent[]): void => {
           expect(classStudents.length).toBe(0);
         });
       await prisma.invite
         .findMany({ where: { classId: classroom.id } })
-        .then((invites) => {
+        .then((invites: Invite[]): void => {
           expect(invites.length).toBe(0);
         });
       await prisma.joinRequest
         .findMany({ where: { classId: classroom.id } })
-        .then((joinRequests) => {
+        .then((joinRequests: JoinRequest[]): void => {
           expect(joinRequests.length).toBe(0);
         });
       await prisma.classAssignment
         .findMany({ where: { classId: classroom.id } })
-        .then((classAssignments) => {
+        .then((classAssignments: ClassAssignment[]): void => {
           expect(classAssignments.length).toBe(0);
         });
     });
-    it("should respond with a `403` status code and a message when the teacher is not associated with the class", async () => {
+    it("should respond with a `403` status code and a message when the teacher is not associated with the class", async (): Promise<void> => {
       // try having a teacher delete a class they are not associated with
       const { status, body } = await request(app)
         .delete(`/class/teacher/${classroom.id}`)
@@ -249,17 +272,17 @@ describe("classroom tests", () => {
       // verfiy that class not deleted
       await prisma.class
         .findUnique({ where: { id: classroom.id } })
-        .then((classroom) => {
+        .then((classroom: Class | null): void => {
           expect(classroom).toBeDefined();
         });
     });
   });
 
-  describe("[GET] /class/teacher/:classId/join-link", () => {
-    it("should respond with a `200` status code and a join link", async () => {
+  describe("[GET] /class/teacher/:classId/join-link", (): void => {
+    it("should respond with a `200` status code and a join link", async (): Promise<void> => {
       // add teacherUser1 to class, so we can test getting the join link
       await addTeacherToClass(teacherUser1.id, classroom.id);
-      const { status, body } = await request(app)
+      let { status, body } = await request(app)
         .get(`/class/teacher/${classroom.id}/join-link`)
         .set("Authorization", `Bearer ${teacherUser1.token}`);
 
@@ -269,27 +292,23 @@ describe("classroom tests", () => {
       );
 
       // also test if the join link works
-      const studentUser = await createStudent(
+      const studentUser: Prisma.UserGetPayload<{
+        include: { student: true };
+      }> & { token: string } = await createStudent(
         "Alice",
         "Anderson",
         "aaaaa@gmail.com",
       );
-      const joinLinkResponse = await request(app)
+      ({ status, body } = await request(app)
         .post(`/join-request/student?joinCode=${classroom.code}`) // can't use body.joinLink here, because the APP_URL is different in the test environment
-        .set("Authorization", `Bearer ${studentUser.token}`);
+        .set("Authorization", `Bearer ${studentUser.token}`));
 
-      expect(joinLinkResponse.status).toBe(201);
-      expect(joinLinkResponse.body).toHaveProperty("joinRequest");
-      expect(joinLinkResponse.body.joinRequest).toHaveProperty(
-        "classId",
-        classroom.id,
-      );
-      expect(joinLinkResponse.body.joinRequest).toHaveProperty(
-        "studentId",
-        studentUser.id,
-      );
+      expect(status).toBe(201);
+      expect(body).toHaveProperty("joinRequest");
+      expect(body.joinRequest).toHaveProperty("classId", classroom.id);
+      expect(body.joinRequest).toHaveProperty("studentId", studentUser.id);
     });
-    it("should respond with a `403` status code and a message when the teacher is not associated with the class", async () => {
+    it("should respond with a `403` status code and a message when the teacher is not associated with the class", async (): Promise<void> => {
       // try getting the join link for a class the teacher is not associated with
       const { status, body } = await request(app)
         .get(`/class/teacher/${classroom.id}/join-link`)
@@ -301,14 +320,14 @@ describe("classroom tests", () => {
       );
       expect(body.joinLink).toBeUndefined();
     });
-    it("should respond with a `404` status code if the class does not exist", async () => {
+    it("should respond with a `404` status code if the class does not exist", async (): Promise<void> => {
       // get an id that isn't used for any existing class in the database
-      const maxClass = await prisma.class.findFirst({
+      const maxClass: Class | null = await prisma.class.findFirst({
         orderBy: {
           id: "desc",
         },
       });
-      const invalidClassId = (maxClass?.id ?? 0) + 1;
+      const invalidClassId: number = (maxClass?.id ?? 0) + 1;
 
       const { status, body } = await request(app)
         .get(`/class/teacher/${invalidClassId}/join-link`)
@@ -320,8 +339,8 @@ describe("classroom tests", () => {
     });
   });
 
-  describe("[PATCH] /class/teacher/:classId/join-link", () => {
-    it("should respond with a `200` status code and a new join link", async () => {
+  describe("[PATCH] /class/teacher/:classId/join-link", (): void => {
+    it("should respond with a `200` status code and a new join link", async (): Promise<void> => {
       // add teacherUser1 to class, so we can test regenerating the join link
       await addTeacherToClass(teacherUser1.id, classroom.id);
       const { status, body } = await request(app)
@@ -330,7 +349,7 @@ describe("classroom tests", () => {
 
       expect(status).toBe(200);
       // verify that join code was updated in database
-      const updatedClass = await prisma.class.findUnique({
+      const updatedClass: Class | null = await prisma.class.findUnique({
         where: { id: classroom.id },
       });
       expect(updatedClass).toBeDefined();
@@ -339,7 +358,7 @@ describe("classroom tests", () => {
         `${APP_URL}/class/teacher/join?joinCode=${updatedClass!.code}`,
       );
     });
-    it("should respond with a `403` status code and a message when the teacher is not associated with the class", async () => {
+    it("should respond with a `403` status code and a message when the teacher is not associated with the class", async (): Promise<void> => {
       const { status, body } = await request(app)
         .patch(`/class/teacher/${classroom.id}/join-link`)
         .set("Authorization", `Bearer ${teacherUser1.token}`); // teacherUser1 is not associated with the class
@@ -349,14 +368,14 @@ describe("classroom tests", () => {
         `Acces denied: Teacher ${teacherUser1.id} is not part of class ${classroom.id}`,
       );
     });
-    it("should respond with a `404` status code if the class does not exist", async () => {
+    it("should respond with a `404` status code if the class does not exist", async (): Promise<void> => {
       // get an id that isn't used for any existing class in the database
-      const maxClass = await prisma.class.findFirst({
+      const maxClass: Class | null = await prisma.class.findFirst({
         orderBy: {
           id: "desc",
         },
       });
-      const invalidClassId = (maxClass?.id ?? 0) + 1;
+      const invalidClassId: number = (maxClass?.id ?? 0) + 1;
 
       const { status, body } = await request(app)
         .patch(`/class/teacher/${invalidClassId}/join-link`)
@@ -367,16 +386,20 @@ describe("classroom tests", () => {
     });
   });
 
-  describe("[GET] /class/teacher/:classId/student", () => {
-    it("should respond with a `200` status code and a list of students", async () => {
+  describe("[GET] /class/teacher/:classId/student", (): void => {
+    it("should respond with a `200` status code and a list of students", async (): Promise<void> => {
       await addTeacherToClass(teacherUser1.id, classroom.id);
       // add some students to the class
-      const studentUser1 = await createStudent(
+      const studentUser1: Prisma.UserGetPayload<{
+        include: { student: true };
+      }> & { token: string } = await createStudent(
         "Steven",
         "Mcclure",
         "velit.dui@hotmail.edu",
       );
-      const studentUser2 = await createStudent(
+      const studentUser2: Prisma.UserGetPayload<{
+        include: { student: true };
+      }> & { token: string } = await createStudent(
         "Omar",
         "Hawkins",
         "lobortis.quam@yahoo.couk",
@@ -415,7 +438,7 @@ describe("classroom tests", () => {
       );
     });
 
-    it("should respond with a `403` status code when the teacher is not associated with the class", async () => {
+    it("should respond with a `403` status code when the teacher is not associated with the class", async (): Promise<void> => {
       // try getting the students for a class the teacher is not associated with
       const { status, body } = await request(app)
         .get(`/class/teacher/${classroom.id}/student`)
