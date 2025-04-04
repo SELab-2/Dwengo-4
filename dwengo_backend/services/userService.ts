ccc602e19420e1ef02ada70bc2d158951681e41b
senne <senne.vandenberge@ugent.be>
2025-04-04 21:20:40 +0200

    user service with central error handling

--- dwengo_backend/services/userService.ts
+++ dwengo_backend/services/userService.ts
@@ -1,6 +1,7 @@
-import { PrismaClient, Role, Student, Teacher, User } from "@prisma/client";
+import { Role, Student, Teacher, User } from "@prisma/client";
 
 import prisma from "../config/prisma";
+import { handlePrismaQuery } from "../errors/errorFunctions";
 
 export default class UserService {
   static async findUser(email: string): Promise<User | null> {
@@ -12,43 +13,55 @@ export default class UserService {
     lastName: string,
     email: string,
     hashedPassword: string,
-    role: Role
+    role: Role,
   ): Promise<User> {
-    return prisma.user.create({
-      data: {
-        firstName,
-        lastName,
-        email,
-        password: hashedPassword,
-        role: role,
-        // This immediately creates the teacher/student/admin records as well
-        teacher: role === Role.TEACHER ? { create: {} } : undefined,
-        student: role === Role.STUDENT ? { create: {} } : undefined,
-        admin: role === Role.ADMIN ? { create: {} } : undefined,
-      },
-      include: {
-        teacher: true,
-        student: true,
-        admin: true,
-      },
-    });
+    return await handlePrismaQuery(() =>
+      prisma.user.create({
+        data: {
+          firstName,
+          lastName,
+          email,
+          password: hashedPassword,
+          role: role,
+          // This immediately creates the teacher/student/admin records as well
+          teacher: role === Role.TEACHER ? { create: {} } : undefined,
+          student: role === Role.STUDENT ? { create: {} } : undefined,
+          admin: role === Role.ADMIN ? { create: {} } : undefined,
+        },
+        include: {
+          teacher: true,
+          student: true,
+          admin: true,
+        },
+      }),
+    );
   }
 
   static async findUserByEmail(email: string): Promise<User> {
-    return prisma.user.findUniqueOrThrow({ where: { email } });
+    return await handlePrismaQuery(() =>
+      prisma.user.findUniqueOrThrow({ where: { email } }),
+    );
   }
 
-  static async findTeacherUserById(userId: number): Promise<(Teacher & { user: User }) | null> {
-    return prisma.teacher.findUnique({
-      where: { userId },
-      include: { user: true },
-    });
+  static async findTeacherUserById(
+    userId: number,
+  ): Promise<(Teacher & { user: User }) | null> {
+    return await handlePrismaQuery(() =>
+      prisma.teacher.findUnique({
+        where: { userId },
+        include: { user: true },
+      }),
+    );
   }
 
-  static async findStudentUserById(userId: number): Promise<(Student & { user: User }) | null> {
-    return prisma.student.findUnique({
-      where: { userId },
-      include: { user: true },
-    });
+  static async findStudentUserById(
+    userId: number,
+  ): Promise<(Student & { user: User }) | null> {
+    return await handlePrismaQuery(() =>
+      prisma.student.findUnique({
+        where: { userId },
+        include: { user: true },
+      }),
+    );
   }
 }
