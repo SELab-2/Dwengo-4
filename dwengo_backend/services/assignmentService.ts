1850e624718a2a410fc4fdca91a74b1dad3e0609
senne <senne.vandenberge@ugent.be>
2025-04-05 14:41:27 +0200

    assignmentService throws error when no assignment found

--- dwengo_backend/services/assignmentService.ts
+++ dwengo_backend/services/assignmentService.ts
@@ -1,16 +1,20 @@
 import { handlePrismaQuery } from "../errors/errorFunctions";
 import { Assignment } from "@prisma/client";
 import prisma from "../config/prisma";
+import { NotFoundError } from "../errors/errors";
 
 export default class AssignmentService {
-  static async getAssignmentById(
-    assignmentId: number
-  ): Promise<Assignment | null> {
-    return await handlePrismaQuery(() =>
+  static async getAssignmentById(assignmentId: number): Promise<Assignment> {
+    const assignment: Assignment | null = await handlePrismaQuery(() =>
       prisma.assignment.findUnique({
         where: { id: assignmentId },
         // geen include meer, want there's no relation to learningPath
-      })
+      }),
     );
+    if (!assignment) {
+      throw new NotFoundError(`Assignment with id ${assignmentId} not found.`);
+    }
+    // There is an assignment
+    return assignment;
   }
 }
