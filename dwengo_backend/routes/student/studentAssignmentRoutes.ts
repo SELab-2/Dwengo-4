88d5912c591535eeb0cd4f63db25eceba74fc01c
senne <senne.vandenberge@ugent.be>
2025-04-06 11:18:48 +0200

    merge conflict anomaly fixed

--- dwengo_backend/routes/student/studentAssignmentRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express, { Router } from "express";
-import { getStudentAssignments } from "../../controllers/student/studentAssignmentController";
-import { protectStudent } from "../../middleware/authMiddleware/studentAuthMiddleware";
-
-const router: Router = express.Router();
-
-router.get("/", protectStudent, getStudentAssignments);
-
-export default router;
