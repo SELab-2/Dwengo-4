e57f62f86b7110ca54c023133ac98f72da486796
senne <senne.vandenberge@ugent.be>
2025-04-07 11:55:37 +0200

    assignment controller update

--- dwengo_backend/controllers/assignmentController.ts
+++ dwengo_backend/controllers/assignmentController.ts
@@ -1,35 +1,22 @@
 import { Request, Response } from "express";
 import assignmentService from "../services/assignmentService";
 import { Assignment } from "@prisma/client";
+import { BadRequestError } from "../errors/errors";
 
 export class AssignmentController {
   async getAssignmentsById(req: Request, res: Response): Promise<void> {
-    try {
-      // 1) Validatie param
-      if (!req.params.assignmentId || isNaN(parseInt(req.params.assignmentId))) {
-        res.status(400).json({ error: "Invalid assignment ID" });
-        return;
-      }
-
-      const assignmentId: number = parseInt(req.params.assignmentId);
-
-      // 2) Opvragen assignment
-      const assignment: Assignment | null = await assignmentService.getAssignmentById(assignmentId);
-
-      // 3) Bestaat het wel?
-      if (!assignment) {
-        res.status(404).json({ error: "Assignment not found" });
-        return;
-      }
-
-      // 4) OK → 200 + assignment
-      res.status(200).json(assignment);
-
-    } catch (error) {
-      res.status(500).json({ error: "Failed to retrieve assignment" });
+    // 1) Validatie param
+    if (!req.params.assignmentId || isNaN(parseInt(req.params.assignmentId))) {
+      throw new BadRequestError("Invalid assignment ID.");
     }
+
+    const assignmentId: number = parseInt(req.params.assignmentId);
+
+    // 2) Opvragen assignment (als er geen error wordt opgegooidd, zijn we zeker dat het bestaat)
+    const assignment: Assignment =
+      await assignmentService.getAssignmentById(assignmentId);
+
+    // 3) OK → 200 + assignment
+    res.status(200).json(assignment);
   }
 }
-
-
-
