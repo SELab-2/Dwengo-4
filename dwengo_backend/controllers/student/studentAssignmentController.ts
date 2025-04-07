98475ac98885df2920524b4dfb6ddb3c042e94bf
senne <senne.vandenberge@ugent.be>
2025-04-07 16:52:12 +0200

    student assignment controller update

--- dwengo_backend/controllers/student/studentAssignmentController.ts
+++ dwengo_backend/controllers/student/studentAssignmentController.ts
@@ -3,48 +3,46 @@ import { getAssignmentsForStudent } from "../../services/studentAssignmentServic
 import { Assignment } from "@prisma/client";
 import { AuthenticatedRequest } from "../../interfaces/extendedTypeInterfaces";
 import { getUserFromAuthRequest } from "../../helpers/getUserFromAuthRequest";
+import asyncHandler from "express-async-handler";
+import { BadRequestError } from "../../errors/errors";
 
 const allowedSortFields: string[] = ["deadline", "createdAt", "updatedAt"];
 
 function extractSortableFields(input: string): string[] {
   return (
-    input?.split(",").filter((field: string): boolean => allowedSortFields.includes(field)) || [
-      "deadline",
-    ]
+    input
+      ?.split(",")
+      .filter((field: string): boolean =>
+        allowedSortFields.includes(field),
+      ) || ["deadline"]
   );
 }
 
-export const getStudentAssignments = async (
-  req: AuthenticatedRequest,
-  res: Response
-): Promise<void> => {
-  try {
+export const getStudentAssignments = asyncHandler(
+  async (req: AuthenticatedRequest, res: Response): Promise<void> => {
     const studentId: number = getUserFromAuthRequest(req).id;
     // Een URL van volgend formaat wordt verwacht
     // GET /assignments?sort=deadline&order=desc&limit=5
 
     // Sorteer standaard de deadline, extra velden kunnen meegegeven worden
-    const sortFields: string[] = extractSortableFields(req.query.sort as string);
+    const sortFields: string[] = extractSortableFields(
+      req.query.sort as string,
+    );
     const order: "desc" | "asc" = req.query.order === "desc" ? "desc" : "asc";
     // Sorteer standaard ascending, descending kan ook
     const limit: number = Number(req.query.limit) || 5;
     // Haal standaard 5 assignments op, andere hoeveelheden kunnen ook
 
     if (isNaN(limit) || limit <= 0) {
-      res.status(400).json({ error: "Limit wasn't a valid number" });
-      return;
+      throw new BadRequestError("Limit must be a positive number.");
     }
 
     const assignments: Assignment[] = await getAssignmentsForStudent(
       studentId,
       sortFields,
       order,
-      limit
+      limit,
     );
     res.status(200).json(assignments);
-  } catch (error) {
-    res
-      .status(500)
-      .json({ error: "Something went wrong while requesting the assignments" });
-  }
-};
+  },
+);
