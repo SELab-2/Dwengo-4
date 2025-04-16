5b962732fd0195cb342c49c8e88e9b903256e010
Cédric Van Cauwenberghe <Cedric.VanCauwenberghe@UGent.be>
2025-04-16 10:51:09 +0200

    Split translation-files

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -1,30 +1,29 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+import translationEN from '../public/locales/en/translation.json';
+import translationNL from '../public/locales/nl/translation.json';
 
 // the translations
 // (tip move them in a JSON file and import them,
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
 const resources = {
   en: {
-    translation: {
-      'Welcome to React': 'Welcome to React and react-i18next',
-    },
+    translation: translationEN,
   },
   nl: {
-    translation: {
-      'Welcome to React': 'Welcome to React and react-i18next',
-      'Kies een rol': 'Kies een rol',
-      student: 'Leerling',
-      teacher: 'Lesgever',
-    },
+    translation: translationNL,
   },
 };
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector) // detect user language
   .init({
     resources,
-    lng: 'nl', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+    supportedLngs: ['nl', 'en'],
+    fallbackLng: 'nl', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
 
