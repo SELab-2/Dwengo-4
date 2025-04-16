02c5416abab612e724faeea89102a02834e1e2eb
Cédric Van Cauwenberghe <Cedric.VanCauwenberghe@UGent.be>
2025-04-16 12:38:37 +0200

    Translations verplaatst

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -2,12 +2,10 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-import translationEN from '../public/locales/en/translation.json';
-import translationNL from '../public/locales/nl/translation.json';
+import translationEN from './locales/en/translation.json';
+import translationNL from './locales/nl/translation.json';
 
 // the translations
-// (tip move them in a JSON file and import them,
-// or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
 const resources = {
   en: {
     translation: translationEN,
@@ -23,9 +21,7 @@ i18n
   .init({
     resources,
     supportedLngs: ['nl', 'en'],
-    fallbackLng: 'nl', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
-    // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
-    // if you're using a language detector, do not define the lng option
+    fallbackLng: 'nl',
 
     interpolation: {
       escapeValue: false, // react already safes from xss
