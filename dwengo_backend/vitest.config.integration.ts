c6999814b95565337d1b96f72149fa0b76a4a56c
hendrik <hendrik.decoster@ugent.be>
2025-04-29 11:22:01 +0200

    testen slagen terug, export default moet blijbaar wel blijven staan in vitest.config.integration.ts

--- dwengo_backend/vitest.config.integration.ts
+++ dwengo_backend/vitest.config.integration.ts
@@ -1,16 +1,16 @@
-/*import { defineConfig } from 'vitest/config'
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    include: ['./tests/!**!/!*.test.ts'],
+    include: ["./tests/**/*.test.ts"],
     poolOptions: {
-        forks: {
-          singleFork: true
-        }
+      forks: {
+        singleFork: true,
+      },
     },
-    setupFiles: ['./tests/helpers/setup.ts'],
+    setupFiles: ["./tests/helpers/setup.ts"],
     coverage: {
-      provider: 'v8'
-    }
-  }
-});*/
+      provider: "v8",
+    },
+  },
+});
