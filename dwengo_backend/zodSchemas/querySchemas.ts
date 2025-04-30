5f90a8e6ed9157d427926b74b5864333df62187d
hendrik <hendrik.decoster@ugent.be>
2025-04-30 15:31:33 +0200

    assignmentRoutes gebruikt nu zod

--- dwengo_backend/zodSchemas/querySchemas.ts
+++ dwengo_backend/zodSchemas/querySchemas.ts
@@ -3,3 +3,5 @@ import { z } from "zod";
 export const limitQuerySchema = z.object({
   limit: z.coerce.number().int().positive().default(5),
 });
+
+export const optionalLimitQuerySchema = limitQuerySchema.optional();
