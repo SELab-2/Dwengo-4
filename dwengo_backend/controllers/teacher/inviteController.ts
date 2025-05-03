ba0c351d58ada65327ab5a1e3f369b13079753f2
Maxim Milet <maxim.milet@ugent.be>
2025-05-03 21:31:08 +0200

    type hints

--- dwengo_backend/controllers/teacher/inviteController.ts
+++ dwengo_backend/controllers/teacher/inviteController.ts
@@ -105,9 +105,9 @@ export const updateInviteStatus = asyncHandler(
 
 export const deleteInvite = asyncHandler(
   async (req: AuthenticatedRequest, res: Response): Promise<void> => {
-    const inviteId = parseInt(req.params.inviteId, 10);
-    const classId  = parseInt(req.params.classId, 10);
-    const classTeacherId = getUserFromAuthRequest(req).id;
+    const inviteId : number = parseInt(req.params.inviteId, 10);
+    const classId : number  = parseInt(req.params.classId, 10);
+    const classTeacherId : number = getUserFromAuthRequest(req).id;
 
     // 1) eerst controleren of invite bestaat en PENDING is
     await inviteService.ensureInviteExists(inviteId, classId);
